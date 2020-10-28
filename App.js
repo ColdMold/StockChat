d7ba8dbb605d5b504269afc042d5e4bc1c66de88
ColdMold <44824348+ColdMold@users.noreply.github.com>
2020-10-27 21:48:41 -0500

    trying to get anonymous sign in working before email/pw

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator  } from '@react-navigation/stack';
 import HomeScreen from './Components/HomeScreen';
+import auth from '@react-native-firebase/auth';
 
 
 const Stack = createStackNavigator();
@@ -11,14 +12,69 @@ const Stack = createStackNavigator();
 export default class App extends React.Component {
   render() {
     return (
+      
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Stock Chat" component={HomeScreen} />
+          <Stack.Screen> component={loginApp}</Stack.Screen>
+          {/*<Stack.Screen name="Stock Chat" component={HomeScreen} /> */}
         </Stack.Navigator>
       </NavigationContainer>
+      /*
+     <View>
+       <loginApp/>
+     </View>
+     */
     );
   }
 
+  componentDidMount() {
+    auth()
+  .signInAnonymously()
+  .then(() => {
+    console.log('User signed in anonymously');
+  })
+  .catch(error => {
+    if (error.code === 'auth/operation-not-allowed') {
+      console.log('Enable anonymous in your firebase console.');
+    }
+
+    console.error(error);
+  });
+  }
+
+}
+
+function loginApp() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
