950989c4fb1869914a2beafbae6324dd596340fb
ColdMold <44824348+ColdMold@users.noreply.github.com>
2020-11-18 22:16:35 -0600

    Update ProfileTab.js

--- Components/AppTabNavigator/ProfileTab.js
+++ Components/AppTabNavigator/ProfileTab.js
@@ -1,15 +1,61 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Title } from 'react-native-paper';
 import FormButton from '../FormButton';
+import FormInput from '../FormInput';
 import { AuthContext } from '../../Navigation/AuthProvider';
+import auth, { firebase } from '@react-native-firebase/auth';
 
 export default function ProfileTab({ navigation }) {
     const { logout, user } = useContext(AuthContext);
-    
+    const [userPassword, setUserPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+
+    reaunthenticate = (currentPassword) => {
+      let cred = firebase.auth.EmailAuthProvider.credential(user.email, currentPassword);
+      //return user.reaunthenticateWithCredential(cred);
+      //return user.reaunthenticateWithCredential(cred);
+      return user.rea
+    }
+
+    changePassword = (currentPassword, newPassword) => {
+      this.reaunthenticate(currentPassword).then(() => {
+        let user = firebase.auth().currentUser;
+        user.updatePassword(newPassword).then(() => {
+          console.log("Password updated");
+        }).catch((error) => {
+          console.log(error);
+        });
+      }).catch((error) => {
+        console.log(error);
+      })
+    }
     return (
         <View style={styles.container}>
           <Title>Welcome {user.displayName}</Title>
+          <FormInput
+          labelName='currentPassword'
+          value={userPassword}
+          autoCapitalize='none'
+          onChangeText={userPassword => setUserPassword(userPassword)}
+        />
+        <FormInput
+          labelName='newPassword'
+          value={newPassword}
+          autoCapitalize='none'
+          onChangeText={newPassword => setNewPassword(newPassword)}
+        />
+          <FormButton
+            title='Update Password'
+            modeValue='contained'
+            labelStyle={styles.loginButtonLabel}
+            onPress={() => {
+                changePassword(userPassword, newPassword);
+                console.log("password updated");
+                //navigation.navigate('Login');
+                //might not need navigation, logout should auto go back to login screen
+            }}
+          />
           <FormButton
             title='Logout'
             modeValue='contained'
