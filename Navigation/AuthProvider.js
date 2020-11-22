5e0c00f6ab28292567e66c310868aba948e985b7
ColdMold <44824348+ColdMold@users.noreply.github.com>
2020-11-21 20:05:09 -0600

    Update AuthProvider.js

--- Navigation/AuthProvider.js
+++ Navigation/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import auth, { firebase } from '@react-native-firebase/auth';
+import auth from '@react-native-firebase/auth';
 
 export const AuthContext = createContext({});
 
