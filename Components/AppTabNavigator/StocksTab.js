43bb91ee670a40ed21cefe9407013f29ed628bc4
ColdMold <44824348+ColdMold@users.noreply.github.com>
2020-12-08 18:41:50 -0600

    Update StocksTab.js
    lists expand and close correctly

--- Components/AppTabNavigator/StocksTab.js
+++ Components/AppTabNavigator/StocksTab.js
@@ -26,7 +26,8 @@ class StocksTab extends Component {
 
     this.state = {
       isLoading: false,
-      isExpanded: true,
+      isFavoritesExpanded: true,
+      isStocksExpanded: true,
       companyInfo: {
         companyNames: [],
         companySymbols: [],
@@ -143,7 +144,7 @@ class StocksTab extends Component {
     let _this = this;
 
     let stocksAccordion = (
-      <List.Accordion title="Stocks" id="stocks" expanded={_this.state.isExpanded} onPress={() => _this.setState(!isExpanded)}>
+      <List.Accordion title="Stocks" id="stocks" expanded={this.state.isStocksExpanded} onPress={() => this.setState({isStocksExpanded: !this.state.isStocksExpanded})}>
         {companyNames.map(function (companyName, index) {
           const companySymbol = companySymbols[index];
           return (
@@ -159,7 +160,7 @@ class StocksTab extends Component {
     );
 
     let favAccordion = (
-      <List.Accordion title="Favorites" id="favorites" expanded={_this.state.isExpanded} onPress={() => _this.setState(!isExpanded)}>
+      <List.Accordion title="Favorites" id="favorites" expanded={this.state.isFavoritesExpanded} onPress={() => this.setState({isFavoritesExpanded: !this.state.isFavoritesExpanded})}>
         {favCompNames.map((compName, index) => {
           const favCompSymbol = favCompSymbols[index];
           return (
